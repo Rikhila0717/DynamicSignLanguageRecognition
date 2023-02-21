5692b62a97e41cb8ef420ca068574a7f61051bf1
Wyshallie <83577359+Wyshallie@users.noreply.github.com>
2023-02-21 16:31:51 +0530

    wysh adding asl files

--- file1.py
+++ file1.py
@@ -8,12 +8,7 @@ import time
 import mediapipe as mp
 from sklearn.model_selection import train_test_split 
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import TensorBoard
 
-
-#keypoints
 
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
@@ -105,64 +100,64 @@ sequence_length = 30
 # Folder start
 start_folder = 30
 
-# for action in actions:
-#     for sequence in range(no_sequences):
-#         try:
-#             os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
-#         except:
-#             pass
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+        except:
+            pass
 
 
-# cap = cv2.VideoCapture(0)
-# # Set mediapipe model 
-# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     
-#     # NEW LOOP
-#     # Loop through actions
-#     for action in actions:
-#         # Loop through sequences aka videos
-#         for sequence in range(no_sequences):
-#             # Loop through video length aka sequence length
-#             for frame_num in range(sequence_length):
+    # NEW LOOP
+    # Loop through actions
+    for action in actions:
+        # Loop through sequences aka videos
+        for sequence in range(no_sequences):
+            # Loop through video length aka sequence length
+            for frame_num in range(sequence_length):
 
-#                 # Read feed
-#                 ret, frame = cap.read()
+                # Read feed
+                ret, frame = cap.read()
 
-#                 # Make detections
-#                 image, results = mediapipe_detection(frame, holistic)
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
 
-#                 # Draw landmarks
-#                 draw_styled_landmarks(image, results)
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
                 
-#                 # NEW Apply wait logic
-#                 if frame_num == 0: 
-#                     cv2.putText(image, 'STARTING COLLECTION', (120,200), 
-#                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
-#                     cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
-#                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-#                     # Show to screen
-#                     cv2.imshow('OpenCV Feed', image)
-#                     cv2.waitKey(500)
-#                 else: 
-#                     cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
-#                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-#                     # Show to screen
-#                     cv2.imshow('OpenCV Feed', image)
+                # NEW Apply wait logic
+                if frame_num == 0: 
+                    cv2.putText(image, 'STARTING COLLECTION', (120,200), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0), 4, cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                    cv2.waitKey(500)
+                else: 
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
+                               cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
                 
-#                 # NEW Export keypoints
-#                 keypoints = extract_keypoints(results)
-#                 npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
-#                 np.save(npy_path, keypoints)
+                # NEW Export keypoints
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
 
-#                 # Break gracefully
-#                 if cv2.waitKey(10) & 0xFF == ord('q'):
-#                     break
+                # Break gracefully
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
                     
-#     cap.release()
-#     cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
-# result_test = extract_keypoints(results)
-# print(result_test)
+result_test = extract_keypoints(results)
+print(result_test)
 
 label_map = {label:num for num, label in enumerate(actions)}
 
@@ -177,23 +172,36 @@ for action in actions:
         labels.append(label_map[action])
 
 print(np.array(sequences).shape)
-print(np.array(labels).shape)
+
+
+
+#preprocess data and collect labels
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+label_map = {label:num for num, label in enumerate(actions)}
+print(label_map)
+
+
+sequences, labels = [], []
+for action in actions:
+    for sequence in np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+print(np.array(sequences).shape)
 
 X = np.array(sequences)
-print(X.shape)
-
 y = to_categorical(labels).astype(int)
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.05)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
-print(y_test.shape)
-
-#build nd train LSTM network
-
-#monitor live training nd accuracy
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
 log_dir = os.path.join('Logs')
 tb_callback = TensorBoard(log_dir=log_dir)
-
-
 model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
 model.add(LSTM(128, return_sequences=True, activation='relu'))
@@ -201,17 +209,11 @@ model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(actions.shape[0], activation='softmax'))
-
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
-print(model.summary())
+model.summary()
 
-res = model.predict(X_test)
 
-#predicted
-print(actions[np.argmax(res[4])])
 
-#original
-print(actions[np.argmax(y_test[4])])
 
 
